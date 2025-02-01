d823db9e3d8869daafcb40b4151a3d4dd0878881
officialBusiness <a634318935@126.com>
2025-02-01 16:56:16 +0800

    添加 phaser3-rex-plugins@1.1.84 依赖

---
+++ libs/phaser3-rex-plugins@1.1.84/plugins/utils/ninepatch/Methods.js
@@ -0,0 +1,20 @@
+import SetGetFrameNameCallback from './texture/SetGetFrameNameCallback.js';
+import SetBaseTexture from './texture/SetBaseTexture.js';
+import UpdateTexture from './texture/UpdateTexture.js';
+import SetStretchMode from './texture/SetStretchMode.js';
+import GetStretchMode from './texture/GetStretchMode.js';
+import SetPreserveRatio from './texture/SetPreserveRatio.js';
+import SetMaxFixedPartScale from './texture/SetMaxFixedPartScale.js';
+import NOOP from '../object/NOOP';
+
+export default {
+    _drawImage: NOOP,
+    _drawTileSprite: NOOP,
+    setGetFrameNameCallback: SetGetFrameNameCallback,
+    setBaseTexture: SetBaseTexture,
+    updateTexture: UpdateTexture,
+    setStretchMode: SetStretchMode,
+    getStretchMode: GetStretchMode,
+    setPreserveRatio: SetPreserveRatio,
+    setMaxFixedPartScale: SetMaxFixedPartScale,
+};
