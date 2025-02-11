3204e06acc5c40cf5df8d213333d6c2e97b29529
officialBusiness <a634318935@126.com>
2025-02-11 20:21:12 +0800

    fabric.start 案例调整

--- github_repositories/fabric.js/examples/fabric.js.modules.js
+++ github_repositories/fabric.js/examples/fabric.js.modules.js
@@ -1,5 +1,5 @@
 import '../fabric.js_mark/fabric.js_mark.js';
-// import * as fabric from '../source_code/dist/fabric.mjs';
-// globalThis.fabric = fabric;
+import * as fabric from '../source_code/dist/fabric.mjs';
+globalThis.fabric = fabric;
 
-codeMarkObject(fabric, 'fabric');
+// codeMarkObject(fabric, 'fabric');
