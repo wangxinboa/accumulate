0414ec2c3fa52f6d609c3b04f4806ec364035391
officialBusiness <a634318935@126.com>
2025-02-12 19:27:52 +0800

    game_engine 调整 text height, width 计算方式

--- tools/game_engine/src/objects/2d/text.js
+++ tools/game_engine/src/objects/2d/text.js
@@ -1,5 +1,9 @@
-import Object2D from "../base/object2d.js";
-import MeasureText from "./measure_text.js";
+import Object2D from "./base/object2d.js";
+
+const _canvas = document.createElement('canvas');
+_canvas.width = 0;
+_canvas.height = 0;
+const _ctx = _canvas.getContext('2d');
 
 export default class Text extends Object2D {
 	constructor(scene, option = {}) {
@@ -15,15 +19,28 @@ export default class Text extends Object2D {
 		this.textBackgroundColor = option.textBackgroundColor || 'transparent';
 		this.backgroundColor = option.backgroundColor || 'transparent';
 
-		MeasureText(this);
+		this._initDimensions();
 
 		this.updateMatrix();
 		this.updateRange();
 	}
 
+	// https://juejin.cn/post/7308697586533974056
+	_initDimensions() {
+		this._setTextStyles(_ctx);
+		const textMetrics = _ctx.measureText(this.text);
+
+		this.left = textMetrics.actualBoundingBoxLeft;
+		this.ascent = textMetrics.actualBoundingBoxAscent;
+		this.descent = textMetrics.actualBoundingBoxDescent;
+
+		this.width = textMetrics.actualBoundingBoxLeft + textMetrics.actualBoundingBoxRight;
+		this.height = textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent;
+	}
+
+
 	_setTextStyles(ctx) {
-		// ctx.textBaseline = 'alphabetic';
-		// ctx.textAlign = this.textAlign;
+		ctx.textBaseline = 'alphabetic';
 		ctx.font = `${this.fontStyle} ${this.fontWeight} ${this.fontSize}px ${this.fontFamily}`;
 	}
 
@@ -39,10 +56,10 @@ export default class Text extends Object2D {
 
 		this._setTextStyles(ctx);
 		if (this.hasFill()) {
-			ctx.fillText(this.text, 0, this.fontSize);
+			ctx.fillText(this.text, this.left, this.ascent);
 		}
 		if (this.hasStroke()) {
-			ctx.strokeText(this.text, 0, this.fontSize);
+			ctx.strokeText(this.text, this.left, this.ascent);
 		}
 	}
 
