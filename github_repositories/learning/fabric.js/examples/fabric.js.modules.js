4256615228ed814b18eb664ee54df3ba341f1939
wangxinboa <a634318935@126.com>
2025-03-24 20:42:54 +0800

    调整 github_repositories/learning/fabric.js

--- github_repositories/learning/fabric.js/examples/fabric.js.modules.js
+++ github_repositories/learning/fabric.js/examples/fabric.js.modules.js
@@ -1,8 +1,6 @@
-import '../../../../libs/lil-gui/dist/lil-gui.umd.js';
+import '../../../../tools/code_analysis/javascript/code_mark/global_api.js';
 
-import '../fabric.js_mark/fabric.js_mark.js';
+import '../../../../libs/lil-gui/dist/lil-gui.umd.js';
 
 import * as fabric from '../source_code/dist/fabric.mjs';
 globalThis.fabric = fabric;
-
-// codeMarkObject(fabric, 'fabric');
