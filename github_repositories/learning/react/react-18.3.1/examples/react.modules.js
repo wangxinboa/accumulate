90d4f26e3e166fd82eb715e6141f9a6ddd9c8404
wangxinboa <a634318935@126.com>
2025-03-25 19:14:09 +0800

    react source 改成 source_code

--- github_repositories/learning/react/react-18.3.1/examples/react.modules.js
+++ github_repositories/learning/react/react-18.3.1/examples/react.modules.js
@@ -1,7 +1,7 @@
 import '../../../../../tools/code_analysis/javascript/code_mark/global_api.js';
 
-import '../source/react.development.js';
-import '../source/react-dom.development.js';
+import '../source_code/react.development.js';
+import '../source_code/react-dom.development.js';
 
-// import '../source/react.production.min.js';
-// import '../source/react-dom.production.min.js';
+// import '../source_code/react.production.min.js';
+// import '../source_code/react-dom.production.min.js';
