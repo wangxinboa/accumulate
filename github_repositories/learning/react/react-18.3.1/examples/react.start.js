698cb0b46fab28b8a1a1fe938d4e14ef1c8fef1d
wangxinboa <a634318935@126.com>
2025-03-27 20:03:34 +0800

    react.start.js 添加模拟事件; button 事件改成 onMouseDown， 打印改成 error

--- github_repositories/learning/react/react-18.3.1/examples/react.start.js
+++ github_repositories/learning/react/react-18.3.1/examples/react.start.js
@@ -3,8 +3,8 @@ const rootNode = document.getElementById('root');
 const App = React.createElement(
 	'button',
 	{
-		onClick: function () {
-			console.log('You liked this!');
+		onMouseDown: function () {
+			console.error('You liked this!');
 		}
 	},
 	'Like'
@@ -22,3 +22,26 @@ const App = React.createElement(
 const root = ReactDOM.createRoot(rootNode);
 
 root.render(App);
+
+
+function mousedown() {
+	// 模拟事件
+	// 获取 dom
+	const button = document.querySelectorAll("button")[0];
+	if (button) {
+		// 创建event
+		const event = document.createEvent("MouseEvents");
+		// 初始化event
+		event.initMouseEvent("mousedown",
+			true, true, document.defaultView, 0,
+			10, 10,// screen
+			10, 10,// client
+			false, false, false, false, 0, null);
+		// 触发事件
+		button.dispatchEvent(event);
+	}
+}
+
+setTimeout(() => {
+	mousedown();
+}, 1000)
