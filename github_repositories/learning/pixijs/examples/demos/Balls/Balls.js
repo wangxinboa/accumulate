4b0153d2d094ead0f54ba3360f01524a8ef4336e
wangxinboa <a634318935@126.com>
2025-04-14 20:05:47 +0800

    pixijs 案例全部转为 js 格式, 删除不必要文件

---
+++ github_repositories/learning/pixijs/examples/demos/Balls/Balls.js
@@ -0,0 +1,94 @@
+window.onorientationchange = resize;
+window.onresize = resize;
+
+document.addEventListener('DOMContentLoaded', start, false);
+
+//	Globals, globals everywhere and not a drop to drink
+var w = 1024;
+var h = 768;
+var starCount = 25;
+var sx = 1.0 + (Math.random() / 20);
+var sy = 1.0 + (Math.random() / 20);
+var slideX = w / 2;
+var slideY = h / 2;
+var stars = [];
+
+var renderer = null;
+var stage = null;
+
+function start() {
+
+	var ballTexture = new PIXI.Texture.fromImage("./demos/Balls/assets/bubble_32x32.png");
+
+	renderer = PIXI.autoDetectRenderer(w, h);
+
+	stage = new PIXI.Stage;
+
+	document.body.appendChild(renderer.view);
+
+	for (var i = 0; i < starCount; i++) {
+		var tempBall = new PIXI.Sprite(ballTexture);
+
+		tempBall.position.x = (Math.random() * w) - slideX;
+		tempBall.position.y = (Math.random() * h) - slideY;
+		tempBall.anchor.x = 0.5;
+		tempBall.anchor.y = 0.5;
+
+		stars.push({ sprite: tempBall, x: tempBall.position.x, y: tempBall.position.y });
+
+		stage.addChild(tempBall);
+	}
+
+	resize();
+
+	requestAnimFrame(update);
+
+}
+
+window.newWave = newWave;
+
+function newWave(x, y) {
+
+	sx = x;
+	sy = y;
+
+}
+
+function resize() {
+	console.info('resize:', resize);
+
+	w = window.innerWidth - 16;
+	h = window.innerHeight - 16;
+
+	slideX = w / 2;
+	slideY = h / 2;
+
+	renderer.resize(w, h);
+}
+
+function update() {
+	for (var i = 0; i < starCount; i++) {
+		stars[i].sprite.position.x = stars[i].x + slideX;
+		stars[i].sprite.position.y = stars[i].y + slideY;
+		stars[i].x = stars[i].x * sx;
+		stars[i].y = stars[i].y * sy;
+
+		if (stars[i].x > w) {
+			stars[i].x = stars[i].x - w;
+		}
+		else if (stars[i].x < -w) {
+			stars[i].x = stars[i].x + w;
+		}
+
+		if (stars[i].y > h) {
+			stars[i].y = stars[i].y - h;
+		}
+		else if (stars[i].y < -h) {
+			stars[i].y = stars[i].y + h;
+		}
+	}
+
+	renderer.render(stage);
+
+	requestAnimFrame(update);
+}
