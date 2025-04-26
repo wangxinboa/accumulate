6c7abfb6bfbe0e9787ff0140e70dce5a5d2417ea
wangxinboa <a634318935@126.com>
2025-04-26 11:44:06 +0800

    pixijs 学习添加源码匹配 6bffbdbaafb2cead567b35bf67aaa1a65b74389b

---
+++ github_repositories/learning/pixijs/source_code/src/pixi/loaders/SpriteSheetLoader.js
@@ -0,0 +1,103 @@
+/**
+ * @author Mat Groves http://matgroves.com/ @Doormat23
+ */
+
+/**
+ * The sprite sheet loader is used to load in JSON sprite sheet data
+ * To generate the data you can use http://www.codeandweb.com/texturepacker and publish the "JSON" format
+ * There is a free version so thats nice, although the paid version is great value for money.
+ * It is highly recommended to use Sprite sheets (also know as texture atlas") as it means sprite"s can be batched and drawn together for highly increased rendering speed.
+ * Once the data has been loaded the frames are stored in the PIXI texture cache and can be accessed though PIXI.Texture.fromFrameId() and PIXI.Sprite.fromFromeId()
+ * This loader will also load the image file that the Spritesheet points to as well as the data.
+ * When loaded this class will dispatch a "loaded" event
+ * @class SpriteSheetLoader
+ * @extends EventTarget
+ * @constructor
+ * @param {String} url the url of the sprite sheet JSON file
+ * @param {Boolean} crossorigin
+ */
+
+PIXI.SpriteSheetLoader = function(url, crossorigin)
+{
+	/*
+	 * i use texture packer to load the assets..
+	 * http://www.codeandweb.com/texturepacker
+	 * make sure to set the format as "JSON"
+	 */
+	PIXI.EventTarget.call(this);
+	this.url = url;
+	this.baseUrl = url.replace(/[^\/]*$/, "");
+	this.texture = null;
+	this.frames = {};
+	this.crossorigin = crossorigin;
+};
+
+// constructor
+PIXI.SpriteSheetLoader.constructor = PIXI.SpriteSheetLoader;
+
+/**
+ * This will begin loading the JSON file
+ */
+PIXI.SpriteSheetLoader.prototype.load = function()
+{
+	this.ajaxRequest = new AjaxRequest();
+	var scope = this;
+	this.ajaxRequest.onreadystatechange = function()
+	{
+		scope.onJSONLoaded();
+	};
+		
+	this.ajaxRequest.open("GET", this.url, true);
+	if (this.ajaxRequest.overrideMimeType) this.ajaxRequest.overrideMimeType("application/json");
+	this.ajaxRequest.send(null)
+};
+
+/**
+ * Invoke when JSON file is loaded
+ * @private
+ */
+PIXI.SpriteSheetLoader.prototype.onJSONLoaded = function()
+{
+	if (this.ajaxRequest.readyState == 4)
+	{
+		 if (this.ajaxRequest.status == 200 || window.location.href.indexOf("http") == -1)
+	 	{
+			var jsonData = eval("(" + this.ajaxRequest.responseText + ")");
+			var textureUrl = this.baseUrl + jsonData.meta.image;
+
+            var image = new PIXI.ImageLoader(textureUrl, this.crossorigin);
+            this.texture = image.texture.baseTexture;
+            var scope = this;
+            image.addEventListener("loaded", function(event) {
+                 scope.onLoaded();
+            });
+
+			var frameData = jsonData.frames;
+			for (var i in frameData)
+			{
+				var rect = frameData[i].frame;
+				if (rect)
+				{
+					PIXI.TextureCache[i] = new PIXI.Texture(this.texture, {x:rect.x, y:rect.y, width:rect.w, height:rect.h});
+					
+					if(frameData[i].trimmed)
+					{
+						PIXI.TextureCache[i].realSize = frameData[i].spriteSourceSize;
+						PIXI.TextureCache[i].trim.x = PIXI.TextureCache[i].realSize.x / rect.w;
+						PIXI.TextureCache[i].trim.y = PIXI.TextureCache[i].realSize.y / rect.h;
+					}
+				}
+   			}
+
+            image.load();
+	 	}
+	}	
+};
+/**
+ * Invoke when all files are loaded (json and texture)
+ * @private
+ */
+PIXI.SpriteSheetLoader.prototype.onLoaded = function()
+{
+    this.dispatchEvent({type: "loaded", content: this});
+};
---
+++ github_repositories/learning/pixijs/source_code/src/pixi/utils/Utils.js
@@ -0,0 +1,76 @@
+/**
+ * Provides requestAnimationFrame in a cross browser way.
+ */
+window.requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+           window.setTimeout(callback, 1000/60);
+         };
+})();
+
+function HEXtoRGB(hex) {
+	return [(hex >> 16 & 0xFF) / 255, ( hex >> 8 & 0xFF) / 255, (hex & 0xFF)/ 255];
+}
+
+/**
+ * Provides bind in a cross browser way.
+ */
+if (typeof Function.prototype.bind != 'function') {
+  Function.prototype.bind = (function () {
+    var slice = Array.prototype.slice;
+    return function (thisArg) {
+      var target = this, boundArgs = slice.call(arguments, 1);
+ 
+      if (typeof target != 'function') throw new TypeError();
+ 
+      function bound() {
+	var args = boundArgs.concat(slice.call(arguments));
+	target.apply(this instanceof bound ? this : thisArg, args);
+      }
+ 
+      bound.prototype = (function F(proto) {
+          proto && (F.prototype = proto);
+          if (!(this instanceof F)) return new F;          
+	})(target.prototype);
+ 
+      return bound;
+    };
+  })();
+}
+
+var AjaxRequest = function()
+{
+	var activexmodes = ["Msxml2.XMLHTTP", "Microsoft.XMLHTTP"] //activeX versions to check for in IE
+	
+	if (window.ActiveXObject)
+	{ //Test for support for ActiveXObject in IE first (as XMLHttpRequest in IE7 is broken)
+		for (var i=0; i<activexmodes.length; i++)
+		{
+			try{
+				return new ActiveXObject(activexmodes[i])
+			}
+   			catch(e){
+    			//suppress error
+   			}
+		}
+	}
+	else if (window.XMLHttpRequest) // if Mozilla, Safari etc
+  	{
+  		return new XMLHttpRequest()
+ 	}
+ 	else
+ 	{
+		return false;
+ 	}
+}
+
+
+
+
+
+
+
