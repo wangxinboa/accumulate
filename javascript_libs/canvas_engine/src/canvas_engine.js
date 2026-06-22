475d318173571e609ba3638afbe623f87b352f91
wangxinboa <a634318935@126.com>
2026-06-22 18:00:31 +0800

    增强 EventSystem 类的事件处理，绑定事件处理函数上下文并注册右键菜单事件监听器，提升用户交互体验

--- javascript_libs/canvas_engine/src/canvas_engine.js
+++ javascript_libs/canvas_engine/src/canvas_engine.js
@@ -1,5 +1,7 @@
 export { Canvas2DEngine } from "./canvas_2d_engine.js";
+export { Vector2 } from "./math/vector2.js";
 export { Render2DNode } from "./render_nodes/2d/render_2d_node.js";
+export { Sprite2D } from "./render_nodes/2d/sprite2d/sprite2d.js";
 export {
 	GlBufferTargetTypeEnum,
 	GlBufferUsageTypeEnum,
@@ -9,6 +11,5 @@ export {
 export { GlAttribs } from "./renderers/webgl_renderer/webgl_buffer/gl_attribs/gl_attribs.js";
 export { GlBuffer } from "./renderers/webgl_renderer/webgl_buffer/gl_attribs/gl_buffer.js";
 export { getPositionUvFloat32ArrayFromWidthAndHeight } from "./renderers/webgl_renderer/webgl_buffer/webgl_buffer_utils.js";
-export { Sprite2D } from "./render_nodes/2d/sprite2d/sprite2d.js";
 export { ImageTexture } from "./textures/image_texture.js";
 export { TextTexture } from "./textures/text_texture.js";
--- javascript_libs/canvas_engine/src/event/event_system.js
+++ javascript_libs/canvas_engine/src/event/event_system.js
@@ -18,17 +18,22 @@ export class EventSystem extends BaseCleanUp {
 
 		this.activeEvent = null;
 
+		// 绑定事件处理函数上下文
 		this.onMouseDown = this.onMouseDown.bind(this);
 		this.onMouseMove = this.onMouseMove.bind(this);
 		this.onMouseUp = this.onMouseUp.bind(this);
 		this.onWheel = this.onWheel.bind(this);
+		this.onContextMenu = this.onContextMenu.bind(this);
 
+		// 注册事件监听器
 		this._domElement.addEventListener("mousedown", this.onMouseDown);
 		window.addEventListener("mousemove", this.onMouseMove);
 		window.addEventListener("mouseup", this.onMouseUp);
 		this._domElement.addEventListener("wheel", this.onWheel, { passive: true });
+		this._domElement.addEventListener("contextmenu", this.onContextMenu);
 	}
 	/**
+	 * 激活2D事件模式
 	 * @param {CanvasEngineType.Scene2D} scene
 	 * @param {CanvasEngineType.Camera2D} camera
 	 */
@@ -36,14 +41,17 @@ export class EventSystem extends BaseCleanUp {
 		this.activeEvent = this.event2DMode.bindScene(scene, camera);
 	}
 	/**
+	 * 处理鼠标按下事件
 	 * @param {MouseEvent} event
 	 */
 	onMouseDown(event) {
+		event.preventDefault();
 		if (this.activeEvent) {
 			this.activeEvent.processDownEvents(event.offsetX, event.offsetY);
 		}
 	}
 	/**
+	 * 处理鼠标移动事件
 	 * @param {MouseEvent} event
 	 */
 	onMouseMove(event) {
@@ -52,6 +60,7 @@ export class EventSystem extends BaseCleanUp {
 		}
 	}
 	/**
+	 * 处理鼠标抬起事件
 	 * @param {MouseEvent} event
 	 */
 	onMouseUp(event) {
@@ -60,6 +69,7 @@ export class EventSystem extends BaseCleanUp {
 		}
 	}
 	/**
+	 * 处理鼠标滚轮事件
 	 * @param {WheelEvent} event
 	 */
 	onWheel(event) {
@@ -67,7 +77,16 @@ export class EventSystem extends BaseCleanUp {
 			this.activeEvent.processWheelEvents(event.deltaX, event.deltaY, event.deltaZ, event.offsetX, event.offsetY);
 		}
 	}
-
+	/**
+	 * 禁止右键菜单弹出
+	 * @param {MouseEvent} event
+	 */
+	onContextMenu(event) {
+		event.preventDefault();
+	}
+	/**
+	 * 销毁事件系统，移除所有事件监听器
+	 */
 	destroy() {
 		this.event2DMode.destroy();
 
@@ -75,6 +94,7 @@ export class EventSystem extends BaseCleanUp {
 		window.removeEventListener("mousemove", this.onMouseMove);
 		window.removeEventListener("mouseup", this.onMouseUp);
 		this._domElement.removeEventListener("wheel", this.onWheel);
+		this._domElement.removeEventListener("contextmenu", this.onContextMenu);
 
 		super.destroy();
 	}
